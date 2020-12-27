97c805e99cf39c8b4520a5b4b27828b4b89b751d
Raeesa Qureshi <qureshi.raeesa@gmail.com>
2020-12-27 00:15:52 +0000

    recipe show action

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -10,10 +10,14 @@ class RecipesController < ApplicationController
   def create
     @recipe = Recipe.new(recipe_params)
     if @recipe.save
-      redirect_to cocktail_path(@recipe)
+      redirect_to recipe_path(@recipe)
     else
       render :new
     end
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
   end
 
   private
